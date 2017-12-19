ccd41a8b8bc80d6c4251e1c248e5de1042d74bd9
usausa <machi.pon@gmail.com>
2017-12-19 09:09:11 +0900

    ValidateAttribute

---
+++ Smart.AspNetCore/AspNetCore/Filters/ValidateAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace Smart.AspNetCore.Filters
+{
+    using System;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ValidateAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestResult();
+            }
+        }
+    }
+}
