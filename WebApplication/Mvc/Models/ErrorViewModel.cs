fe1f1ebf1e460d99bde4c51f089be7896e6c5001
usausa <machi.pon@gmail.com>
2018-06-04 15:35:24 +0900

    Update example project

--- WebApplication/Mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication.Mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
