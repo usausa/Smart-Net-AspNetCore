ca0c925986b517ef5be346d67e181f7cdba85947
usausa <machi.pon@gmail.com>
2018-10-07 18:25:32 +0900

    Add filter

---
+++ Smart.AspNetCore/AspNetCore/Filters/NotFoundResultFilterAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Smart.AspNetCore.Filters
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public sealed class NotFoundResultFilterAttribute : ResultFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (context.Result is ObjectResult objectResult && objectResult.Value == null)
+            {
+                context.Result = new NotFoundResult();
+            }
+        }
+    }
+}
