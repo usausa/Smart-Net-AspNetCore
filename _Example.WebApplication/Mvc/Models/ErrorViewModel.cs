68ab6eff248c9bb2b9ad4b86172365a700b33f0b
usausa <machi.pon@gmail.com>
2018-12-17 22:34:21 +0900

    Remove old project

--- _Example.WebApplication/Mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Example.WebApplication.Mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
