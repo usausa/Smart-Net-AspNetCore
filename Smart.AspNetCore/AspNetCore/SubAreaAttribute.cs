fd40b685d4928f1e8a37c9f84128fccf6047c8da
usausa <machi.pon@gmail.com>
2019-01-14 19:01:57 +0900

    Add sub area view location expander

---
+++ Smart.AspNetCore/AspNetCore/SubAreaAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Smart.AspNetCore
+{
+    using System;
+
+    using Microsoft.AspNetCore.Mvc.Routing;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class SubAreaAttribute : RouteValueAttribute
+    {
+        public SubAreaAttribute(string routeValue)
+            : base("subarea", routeValue)
+        {
+        }
+    }
+}
