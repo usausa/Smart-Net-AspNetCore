ec09421cc00b9a26086e777a1413fa594b2d23b0
usausa <machi.pon@gmail.com>
2022-07-23 14:43:35 +0900

    Add result

---
+++ Smart.AspNetCore/AspNetCore/Mvc/PushStreamResult.cs
@@ -0,0 +1,28 @@
+namespace Smart.AspNetCore.Mvc;
+
+using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+public class PushStreamResult : FileResult
+{
+    private readonly Func<Stream, Task> callback;
+
+    private readonly string filename;
+
+    public PushStreamResult(string contentType, string filename, Func<Stream, Task> callback)
+        : base(contentType)
+    {
+        this.callback = callback;
+        this.filename = filename;
+    }
+
+    public override async Task ExecuteResultAsync(ActionContext context)
+    {
+        var response = context.HttpContext.Response;
+        response.Headers["Content-Disposition"] = $"attachment; filename= {filename}";
+        response.ContentType = ContentType;
+        await callback(context.HttpContext.Response.Body);
+    }
+}
