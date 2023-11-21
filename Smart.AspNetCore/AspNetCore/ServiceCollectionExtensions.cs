a89eeaa23dcad3f21d8d19e680d25620c5ac369e
usausa <machi.pon@gmail.com>
2023-11-21 13:48:58 +0900

    Update extensions

--- Smart.AspNetCore/AspNetCore/ServiceCollectionExtensions.cs
+++ Smart.AspNetCore/AspNetCore/ServiceCollectionExtensions.cs
@@ -1,5 +1,10 @@
 namespace Smart.AspNetCore;
 
+using System.Net;
+using System.Net.Sockets;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -63,5 +68,48 @@ public static class ServiceCollectionExtensions
     public static IFilterMetadata AddTimeLogging(this FilterCollection filters)
     {
         return filters.AddService<TimeLoggingFilter>();
+    }
+
+    //--------------------------------------------------------------------------------
+    // Path restrict
+    //--------------------------------------------------------------------------------
+
+    public static IApplicationBuilder UseWhenFrom(this IApplicationBuilder builder, string path, string[]? networks, Action<IApplicationBuilder> configuration)
+    {
+        if ((networks is null) || (networks.Length == 0))
+        {
+            bool Predicate(HttpContext context)
+            {
+                return context.Request.Path.StartsWithSegments(path, StringComparison.OrdinalIgnoreCase);
+            }
+
+            builder.UseWhen(Predicate, configuration);
+        }
+        else
+        {
+            var ipNetworks = networks
+                .Select(static x =>
+                {
+                    if (IPNetwork.TryParse(x, out var network))
+                    {
+                        return network;
+                    }
+
+                    var address = IPAddress.Parse(x);
+                    return new IPNetwork(address, address.AddressFamily == AddressFamily.InterNetwork ? 32 : 128);
+                })
+                .ToArray();
+
+            bool Predicate(HttpContext context)
+            {
+                return context.Request.Path.StartsWithSegments(path, StringComparison.OrdinalIgnoreCase) &&
+                       Array.Exists(ipNetworks, network => (context.Request.HttpContext.Connection.RemoteIpAddress is not null) &&
+                                                           network.Contains(context.Request.HttpContext.Connection.RemoteIpAddress));
+            }
+
+            builder.UseWhen(Predicate, configuration);
+        }
+
+        return builder;
     }
 }
