479d21d4212dcd0e0519c998b6d8f9bd9b7830a5
usausa <machi.pon@gmail.com>
2026-09-02 19:16:00 +0900

    Update

--- Smart.AspNetCore.Tests/Generator/CompilationHelper.cs
+++ Smart.AspNetCore.Tests/Generator/CompilationHelper.cs
@@ -23,7 +23,7 @@ internal static class CompilationHelper
         var result = Runner.Run(source);
 
         return new GeneratorResult(
-            [.. result.GeneratorDiagnostics],
+            result.GeneratorDiagnostics.ToImmutableArray(),
             result.GeneratedSources,
             result.AllGeneratedText);
     }
